ec8b5d2c95fde882dc301b79cda0982f18b49f3f
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-01-04 20:19:04 +0100

    Create playRound function

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-// current step 3. Make a function that randomly return
-// rock paper or scissors.
+// current step 4. write a function that plays a single
+// round of rock paper scissors
 
 function computerPlay() {
   let answer;
@@ -11,8 +11,32 @@ function computerPlay() {
   } else {
     answer = "Scissors";
   }
-  console.log(answer);
   return answer;
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+  let result;
+  playerSelection = playerSelection.toUpperCase();
+  if (playerSelection === "ROCK" && computerSelection === "Rock") {
+    result = "Draw! You both did Rock";
+  } else if (playerSelection === "ROCK" && computerSelection === "Scissors") {
+    result = "You win! Rock beats Scissors";
+  } else if (playerSelection === "ROCK" && computerSelection === "Paper") {
+    result = "You loose! Paper beats Rock";
+  } else if (playerSelection === "PAPER" && computerSelection === "Rock") {
+    result = "You win! Paper beats Rock";
+  } else if (playerSelection === "PAPER" && computerSelection === "Paper") {
+    result = "Draw! You both did Paper";
+  } else if (playerSelection === "PAPER" && computerSelection === "Scissors") {
+    result = "You loose! Scissors beats Paper";
+  } else if (playerSelection === "SCISSORS" && computerSelection === "Rock") {
+    result = "You loose! Rock beats Scissors";
+  } else if (playerSelection === "SCISSORS" && computerSelection === "Paper") {
+    result = "You win! Scissors beats Paper";
+  } else if (playerSelection === "SCISSORS" && computerSelection === "Scissors") {
+    result = "Draw! You both did Scissors";
+  } else {
+    result = "Please enter rock paper or scissors";
+  }
+  return result;
+}
